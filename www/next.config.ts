01b58d9e00dfb02a516e1bb9377b95f457af1f34
Lukas Diegelmann <lukasdiegelmann@icloud.com>
2024-12-30 22:56:43 +0100

    Added SCSS Support and Tailwind Support

--- www/next.config.ts
+++ www/next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // SASS/SCSS Options
+  sassOptions: {
+    // Here you can add additional SASS Code that will always be added
+    // into the SASS compilation process.
+    additionalData: "",
+  },
 };
 
 export default nextConfig;
