1b92bed4fda6f0ef00469f045dfd82cfc15448a5
Lukas Diegelmann <lukasdiegelmann@icloud.com>
2024-12-31 17:56:54 +0100

    Started creating the Navbar component, Added new Favicon

---
+++ www/src/components/navbar/navbar.tsx
@@ -0,0 +1,18 @@
+/*
+ * This will be the component for the homepage Navbar. Not to be confused by the
+ * Toolbar that will be used in the timeline editor.
+ */
+
+import styles from "./navbar.module.scss"
+
+import { FunctionComponent } from "react"
+
+const Navbar: FunctionComponent = () => {
+    return (
+        <div className={styles["navbar"]}>
+            <div></div>
+        </div>
+    )
+}
+
+export default Navbar
