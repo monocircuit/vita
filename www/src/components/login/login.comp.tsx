83586adc0554bb48cecea83cafdc5db97b2fc5a2
Lukas Diegelmann <lukasdiegelmann@icloud.com>
2025-03-16 21:46:15 +0100

    merge navbar main

--- www/src/components/login/login.comp.tsx
+++ www/src/components/login/login.comp.tsx
@@ -1,16 +1,16 @@
 /** @format */
-"use client";
+"use client"
 
-import Input from "@/utils/components/input/input.utils";
-import React, { useState } from "react";
+import Input from "@/utils/components/input/input.utils"
+import React, { useState } from "react"
 
 interface loginData {
-    username?: string;
-    password?: string;
+    username?: string
+    password?: string
 }
 
 function LoginComp() {
-    const [input, setInput] = useState<loginData>({});
+    const [input, setInput] = useState<loginData>({})
     return (
         <div className="flex h-full justify-center items-center">
             <div className="border-black border-2 rounded-lg w-[500px] h-[600px] m-2 flex justify-center items-center flex-col">
@@ -21,7 +21,7 @@ function LoginComp() {
                 <div className="flex-1"></div>
             </div>
         </div>
-    );
+    )
 }
 
-export default LoginComp;
+export default LoginComp
