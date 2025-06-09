9c2ae7ecb4fd959cbac7761593928c04762e5ae6
BenediktNau <bene.nau27@gmail.com>
2025-06-09 14:15:48 +0200

    chronicle Fetching

--- www/src/app/dashboard/page.tsx
+++ www/src/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { getChronicles } from "@/utils/supabase/api/chronicle/getChronicles";
 import { getOwnProfile } from "@/utils/supabase/api/profiles/getOwnProfile";
 import { createClient } from "@/utils/supabase/client";
 import { useEffect } from "react";
@@ -12,10 +13,15 @@ const Page = () => {
     const profile = await getOwnProfile();
     console.log(profile);
   }
+  const showChronicles = async () => {
+    const chronicles = await getChronicles();
+    return chronicles
+  }
 
   showProfile();
 
-  console.log()
+  const test = showChronicles();
+  console.log(test)
 
   return <div className="size-full">Dashboard</div>;
 };
